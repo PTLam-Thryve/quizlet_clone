8766200b6dcb38ba5fe507ed0b78f9d9d6d4d4ae
Megadebt <eternalmachine123@gmail.com>
2025-06-05 11:56:06 +0200

    changes made in this commit:
    Finished the QuizEnd screen, now display your result
    properly with appropriate messages.

--- lib/ui/pages/quiz_game_end_page.dart
+++ lib/ui/pages/quiz_game_end_page.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
-class QuizGameEndPage extends StatelessWidget {
+class QuizGameEndPage extends StatefulWidget {
   const QuizGameEndPage(
-      {required this.correctAmount, required this.incorrectAmount, super.key});
+      {required this.correctAmount,
+      required this.incorrectAmount,
+      required this.totalQuestions,
+      super.key});
   final int correctAmount;
   final int incorrectAmount;
+  final int totalQuestions;
+
+  @override
+  State<QuizGameEndPage> createState() => _QuizGameEndPageState();
+}
+
+class _QuizGameEndPageState extends State<QuizGameEndPage> {
+  String resultMessage() {
+    if (widget.correctAmount == widget.totalQuestions) {
+      return 'Flawless victory!';
+    } else if (widget.correctAmount == 0) {
+      return 'Oh...';
+    } else {
+      return 'Better luck next time!';
+    }
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -12,12 +31,36 @@ class QuizGameEndPage extends StatelessWidget {
           title: const Text('Results'),
         ),
         body: Column(
-          spacing: 35,
+          spacing: 40,
           children: [
-            Center(
+            const Center(
               child: Text(
-                  'Quiz result summary: $correctAmount correct, $incorrectAmount incorrect'),
+                'Result summary: ',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
+            Center(
+              child: Container(
+                margin: const EdgeInsets.all(20),
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.green.withAlpha(50),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Text(
+                  '${widget.correctAmount}/${widget.totalQuestions} correct!',
+                  style: const TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+            ),
+            Center(
+              child: Text(resultMessage()),
+            )
           ],
         ),
       );
